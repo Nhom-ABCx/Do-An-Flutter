aa51936902ebe50f64d5a4d29d864b1c5a7fb63d
wwwwww3q <phatdatfbi@gmail.com>
2021-11-09 16:42:07 +0700

    tao trang Dang Ky

---
+++ lib/Pages/signin_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; //text input
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final input1 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            padding: const EdgeInsets.only(top: 100),
+            child: Column(
+              children: [
+                const FlutterLogo(
+                  size: 100,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    top: 10,
+                    right: 20,
+                    bottom: 10,
+                  ),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Username",
+                      hintText: "info@example.com",
+                      prefixIcon: Icon(Icons.supervisor_account),
+                    ),
+                    controller: input1, //gan gia tri cua text vao bien'
+                    textInputAction: TextInputAction.next,
+                    inputFormatters: <TextInputFormatter>[
+                      LengthLimitingTextInputFormatter(255), //gioi han do dai`
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    top: 10,
+                    right: 20,
+                    bottom: 10,
+                  ),
+                  child: TextField(
+                    obscureText: true, //hien * khi nhap text
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Password",
+                      prefixIcon: Icon(Icons.vpn_key),
+                    ),
+                    controller: input1, //gan gia tri cua text vao bien'
+                    inputFormatters: <TextInputFormatter>[
+                      LengthLimitingTextInputFormatter(255), //gioi han do dai`
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  width: 300,
+                  child: TextButton(
+                    child: const Text('Log In',
+                        style: TextStyle(fontSize: 20, color: Colors.white)),
+                    onPressed: () {
+                      //Navigator.pushNamed(context, 'Sign_In');
+                    },
+                  ),
+                ),
+                TextButton(
+                  child: const Text(
+                    "Forgot Password?",
+                    style: TextStyle(fontSize: 17, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Didn't have an account?"),
+                    TextButton(
+                      child: const Text(
+                        "Register Now",
+                        style: TextStyle(fontSize: 17, color: Colors.white),
+                      ),
+                      onPressed: () => Navigator.pushNamed(context, '/Sign_Up'),
+                    ),
+                  ],
+                ),
+                TextButton(
+                  child: const Text(
+                    "VIEW AS GUES",
+                    style: TextStyle(fontSize: 17, color: Colors.amber),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            )),
+      );
+}
---
+++ lib/Pages/wellcome_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class WellcomePage extends StatefulWidget {
+  const WellcomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<WellcomePage> createState() => _WellcomePageState();
+}
+
+class _WellcomePageState extends State<WellcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Align(
+            //alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Text(
+                  'Welcome!',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 32, bottom: 16),
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  width: 250,
+                  child: TextButton(
+                    child: const Text('Sign In',
+                        style: TextStyle(fontSize: 20, color: Colors.white)),
+                    onPressed: () => Navigator.pushNamed(context, '/Sign_In'),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 0, bottom: 16),
+                  decoration: BoxDecoration(
+                    // color: Colors.green,
+                    border: Border.all(color: Colors.green),
+                    // shape: BoxShape.rectangle,
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  ),
+                  width: 250,
+                  child: TextButton(
+                    child: const Text('Sign Up',
+                        style: TextStyle(fontSize: 20, color: Colors.green)),
+                    onPressed: () => Navigator.pushNamed(context, '/Sign_Up'),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    const Text(
+                      'Language : ',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.normal),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'English',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
