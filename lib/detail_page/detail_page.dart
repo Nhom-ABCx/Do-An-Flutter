f81c7ae24f952028711895d82b48b300aac6a58f
wwwwww3q <phatdatfbi@gmail.com>
2021-11-19 13:43:17 +0700

    a

--- lib/detail_page/detail_page.dart
+++ lib/detail_page/detail_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Pages extends StatefulWidget{
+class Pages extends StatefulWidget {
   const Pages({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState()=>PagesState();
-
+  State<StatefulWidget> createState() => PagesState();
 }
-class PagesState extends State<Pages>{
-  @override 
-  Widget build(BuildContext context){
+
+class PagesState extends State<Pages> {
+  @override
+  Widget build(BuildContext context) {
     return const Text('trang page');
   }
 }
