252e996f89a631d73c63396f5b0e0096b57901e8
wwwwww3q <phatdatfbi@gmail.com>
2022-01-09 20:46:04 +0700

    fix Chatlog

--- lib/View/Pages/chat_page.dart
+++ lib/View/Pages/chat_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Modals/message.dart';
 import 'package:flutter_application_1/all_page.dart';
 
-const String _name = "Hanh Vu";
-
 //code layout tham khao tu` google https://viblo.asia/p/flutter-viet-ung-dung-chat-voi-flutter-p1-GrLZD8GOZk0
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -12,7 +10,7 @@ class ChatPage extends StatefulWidget {
   State createState() => ChatPageState();
 }
 
-class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
+class ChatPageState extends State<ChatPage> {
   final List<ChatMessage> _messages = [];
   final TextEditingController _textController = TextEditingController();
   bool _isComposing = false; //dang nhap chu~=false
@@ -20,9 +18,6 @@ class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
 
   @override
   void dispose() {
-    for (ChatMessage message in _messages) {
-      message.animationController.dispose();
-    }
     super.dispose();
   }
 
@@ -107,17 +102,10 @@ class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
     if (text.isNotEmpty) {
       //neu' textField ko rong~ thi` tao 1 widget tin nhan' moi'
       ChatMessage message = ChatMessage(
-        text: text, //noi dung tin nhan'
-        //chuyen dong tin nhan' khi gui~
-        animationController: AnimationController(
-          duration: const Duration(milliseconds: 500),
-          vsync: this,
-        ),
+        message: Message(), //noi dung tin nhan'
       );
       //them vao` danh sach tin nhan' o phan tu? dau` tien
       setState(() => _messages.insert(0, message));
-      //bat dau`animation chay chu~ khi gui~ tin nhan' len
-      message.animationController.forward();
     }
   }
 
@@ -125,18 +113,29 @@ class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:
-          AppBar(iconTheme: const IconThemeData(color: Colors.indigo), title: const Text('Chat with $_name', style: TextStyle(color: Colors.indigo))),
+          AppBar(iconTheme: const IconThemeData(color: Colors.indigo), title: const Text('Chat with ADMIN', style: TextStyle(color: Colors.indigo))),
       body: Column(
         children: [
           Flexible(
             // Flexible dua theo widget, size cua thiet bi ma` thay doi?
-            child: ListView.builder(
-              controller: _controller,
-              padding: const EdgeInsets.all(8.0),
-              reverse: true, //tu duoi' len
-              itemCount: _messages.length,
-              itemBuilder: (context, index) => _messages[index],
-            ),
+            child: FutureBuilder<List<Message>>(
+                future: api_GetAll_Message_Admin(Auth.khachHang.id!),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text(snapshot.error.toString()));
+                  }
+                  return snapshot.hasData
+                      ? ListView.builder(
+                          controller: _controller,
+                          padding: const EdgeInsets.all(8.0),
+                          reverse: true, //tu duoi' len
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) => ChatMessage(message: snapshot.data![index]),
+                        )
+                      : const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                }),
           ),
           // Divider(height: 1.0),
           Container(
@@ -154,38 +153,47 @@ class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
 }
 
 class ChatMessage extends StatelessWidget {
-  const ChatMessage({Key? key, required this.text, required this.animationController}) : super(key: key);
-  final String text;
-  final AnimationController animationController;
+  final Message message;
+  const ChatMessage({Key? key, required this.message}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return SizeTransition(
-      //SizeTransition la` animation chuyen? dong
-      //sizeFactor nhu kieu khai bao'
-      sizeFactor: CurvedAnimation(parent: animationController, curve: Curves.easeOut),
-      //axisAlignment: 1.0,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(right: 16.0),
-              child: CircleAvatar(child: Text(_name[0])), //hinh anh avt
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(_name), //ten
-                Container(
-                  width: MediaQuery.of(context).size.width / (1.3),
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: Text(text), //noi dung chat
-                ),
-              ],
-            ),
-          ],
-        ),
+    //neu la` khach hang nhan'
+    if (message.khachHangId != null) {
+      return Align(
+        alignment: Alignment.centerRight,
+        child: Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width / (1.5)),
+            decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)), color: Colors.blue),
+            child: Text(message.body!, style: const TextStyle(color: Colors.white))), //noi dung chat
+      );
+    }
+    //neu la admin nhan'
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: const CircleAvatar(child: Text("A")), //hinh anh avt
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Admin"), //ten
+              Container(
+                //width: MediaQuery.of(context).size.width / (1.3),
+                constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width / (1.3)),
+                margin: const EdgeInsets.only(top: 5.0),
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                decoration: BoxDecoration(borderRadius: const BorderRadius.all(Radius.circular(30)), color: Colors.grey.withOpacity(0.4)),
+                child: Text(message.body!), //noi dung chat
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
